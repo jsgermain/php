605135f78ef53272b0bdb00292b629e6d2d8b64b
john <john-88@live.fr>
2019-10-01 21:21:31 +0200

    Declaration des variable et echo

---
+++ variable.php
@@ -0,0 +1,17 @@
+<?php
+
+/* declaration et affection d'une variable */
+$nom = 'Jean';
+$prenom = 'Faby';
+
+$note1 = 20;
+$note2 = 15;
+$note3 = 5;
+$note4 = 12;
+
+$moyenne = ($note1 + $note2 + $note3 + $note4)/4;
+
+echo " Bonjour Madame $nom, Permettez-moi de vous appeler $prenom. Vous avez une moyenne de $moyenne 20 lors du dernier control";
+
+
+?>
