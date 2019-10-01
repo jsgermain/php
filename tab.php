c457c5d20c72b046d6bc0d6411d87ecc56bb3089
john <john-88@live.fr>
2019-10-02 00:13:49 +0200

    creation variable et récupération

---
+++ tab.php
@@ -0,0 +1,12 @@
+<?php
+/*
+creation de la variable
+chaque variable se separe par un virgule  
+*/
+$notes = [10, 15, 7, 9];
+
+// Pour récupérer la troisième note du tableau
+echo $notes[2];
+
+
+?>
