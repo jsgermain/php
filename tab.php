67f5e5a2aeb22748cceab125e394933042113879
john <john-88@live.fr>
2019-10-02 00:46:00 +0200

    changer la valeur une variable et assignation de la nouvelle valeur

--- tab.php
+++ tab.php
@@ -7,6 +7,11 @@ $madame = ['nom' => 'Jean',
 'notes' => [10,15,14]
 ];
 
+/*
+pour changer le nom de la personne
+on réupère la valeur de la variable et on assigne la nouvelle valeur
+*/
+$madame['nom'] = 'Saint Germain';
 // Pour  afficher son prenom
 echo $madame['prenom']. ' '. $madame['nom'];
 
