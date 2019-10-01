9003a7f0a310400584d149a51a4da965e3854863
john <john-88@live.fr>
2019-10-01 21:35:42 +0200

    echo par concatenation entre deux variables string

--- variable.php
+++ variable.php
@@ -4,14 +4,8 @@
 $nom = 'Jean';
 $prenom = 'Faby';
 
-$note1 = 20;
-$note2 = 15;
-$note3 = 5;
-$note4 = 12;
-
-$moyenne = ($note1 + $note2 + $note3 + $note4)/4;
-
-echo " Bonjour Madame $nom, Permettez-moi de vous appeler $prenom. Vous avez une moyenne de $moyenne 20 lors du dernier control";
+// echo par concatenation entre deux variables string
+echo  $nom. " ".$prenom;
 
 
 ?>
