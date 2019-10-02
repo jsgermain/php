687bb9318d74d4b65a47317d9f7ce36b42461f70
john <john-88@live.fr>
2019-10-02 15:47:49 +0200

    les feu vert, orange et rouge avec switch case

--- condition.php
+++ condition.php
@@ -1,21 +1,24 @@
 <?php
 // demander à l'utilisateur de rentrer des données 
-$feu = (int)readline("Taper 1: feu vert, 2: feu orange et 3 : feu rouge "); // on castre la valeur entrée en entier
+$feu = (int)readline("Taper 1: feu vert, 2: feu orange et 3 : feu rouge ");
+switch($feu)
+{
+    case 1: 
+        echo  "On peut passer car le feu est vert";
+        break; // permet de ne pas éxucuter la suite
 
-if($feu===1)
-{
-    echo  "On peut passer car le feu est vert";
+    case 2:
+        echo "Avancez prudemment, soyez prêt à arrêter car le feu est orange";
+        break;
+
+    case 3:
+        echo "arretez, danger car le feu est rouge";
+        break;
+
+    default;
+        echo "Commande eronnée";
 }
-elseif($feu===2)
-{
-    echo "Avancez prudemment, soyez prêt à arrêter car le feu est orange";
-}
-elseif($feu===3)
-{
-    echo "arretez, danger car le feu est rouge";
-}
-else
-{
-    echo "Commande eronnée";
-}
+
+
+
 ?>
