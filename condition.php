d1f01370300fcfd5ad55b597908e5e1c13825800
john <john-88@live.fr>
2019-10-02 16:06:09 +0200

    heure ou le magasin est fermé ou ouvert avec la commande (&&) et (||)

--- condition.php
+++ condition.php
@@ -1,24 +1,14 @@
 <?php
 // demander à l'utilisateur de rentrer des données 
-$feu = (int)readline("Taper 1: feu vert, 2: feu orange et 3 : feu rouge ");
-switch($feu)
+$heure = (int)readline("Quelle heure est il? ");
+
+if(($heure >=9 && $heure <12) || ($heure>=14 && $heure<17))
 {
-    case 1: 
-        echo  "On peut passer car le feu est vert";
-        break; // permet de ne pas éxucuter la suite
-
-    case 2:
-        echo "Avancez prudemment, soyez prêt à arrêter car le feu est orange";
-        break;
-
-    case 3:
-        echo "arretez, danger car le feu est rouge";
-        break;
-
-    default;
-        echo "Commande eronnée";
+    echo "Le magasin est ouvert";
 }
-
-
+else
+{
+    echo "Le magasin est fermé";
+}
 
 ?>
