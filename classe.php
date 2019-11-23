16c9e6d1a30fdb08a29856e68631ba843b555249
john <john-88@live.fr>
2019-11-23 09:48:52 +0100

    instanciation de la classe

--- classe.php
+++ classe.php
@@ -5,7 +5,7 @@ class InfoDateRenseigner
     public $dateJour;
     public $mois;
     public $annee;
-
+/* La methode ou la fonction de la classe */
     public function __construct (string $jour, int $dateJour,string $mois,int $annee) 
     {
         $this->jour=$jour;
@@ -13,8 +13,13 @@ class InfoDateRenseigner
         $this->mois=$mois;
         $this->annee=$annee;
     }
-
 }
+/*
+test 
+$InfoDateRenseigner est une nouvelle intance de InfoDateRenseigner()
+*/
+$InfoDateRenseigner = new InfoDateRenseigner('Lundi',5,'Septembre', 1988);
+var_dump($InfoDateRenseigner);
 
 ?>
 /*https://www.youtube.com/watch?v=HUhjN12HZR0&list=PLjwdMgw5TTLVDv-ceONHM_C19dPW1MAMD&index=25&t=0s */
