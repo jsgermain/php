6daea3350f8222338c02655a68f920679633ca59
john <john-88@live.fr>
2019-11-23 12:37:31 +0100

    Fonction (array_pusch) permettant d'ajouter de nouvel élément dans un tableau

--- fonction.php
+++ fonction.php
@@ -1,13 +1,15 @@
 <?php
 $notes = [20, 10, 13];
+array_push($notes,[15, 6]); // Ajouter de nouvel élément dans le tableau
 
 $sum = array_sum($notes); // calcule la somme d'un tableau
 $taille = sizeof($notes); // calcule la taille d'un tableau 
 $count = count($notes); // compter le nombre d'élément d'un tableau 
 
-$moy = round($sum / $count); // arrondir en chiffre rond
-//$moy = round($sum / $count, 2); // arrondir avec deux chiffre après la virgule
+
+$moy = $sum / $count;
 
 echo "La moyenne de vos notes est $moy";
+
 
 ?>
