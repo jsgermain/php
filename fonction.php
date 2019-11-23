4297aec761180a6e9e8c93e85cef7f2f4f26116a
john <john-88@live.fr>
2019-11-23 15:02:04 +0100

    Fonction (exit) permet de forcer la sortie du programme

--- fonction.php
+++ fonction.php
@@ -1,8 +1,23 @@
 <?php
-$notes = [20, 10, 13, 54, 5];
-$notes[]=15;
 
-print_r($notes);
+while(true)
+{
+    $mot = readline("Entrez votre mot : \n");
+    if($mot=='')
+    {
+        exit("Fin de programme"); // exit permet de forcer la sortie du programme
+    }
+    $motreverse = strtolower(strrev($mot));
+    if(strtolower($mot) === $motreverse)
+    {
+        echo " Ce mot est un palindrome \n";
+    }
+    else
+    {
+        echo " Ce mot n'est pas un palindrome\n";
+    }
+    
+}
 
 
 ?>
