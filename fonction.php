4740fcedd4d45b033632aeec8ebc9a71bc6b8eff
john <john-88@live.fr>
2019-11-23 14:45:45 +0100

    Fonction (sort) permet d'organiser l'ordre des éléments d'un tableau

--- fonction.php
+++ fonction.php
@@ -1,9 +1,8 @@
 <?php
 $notes = [20, 10, 13, 54, 5];
-$notesInverser = array_reverse($notes); // sert à inverser l'ordre des élément dans un tableau
+sort($notes); // sert à organiser un tableau
 
 print_r($notes);
-print_r($notesInverser);
 
 
 ?>
