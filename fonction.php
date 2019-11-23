92b2e0b9e6d910b8ce15e5ea7f0092c9c4d3a642
john <john-88@live.fr>
2019-11-23 12:22:17 +0100

    Fontion (round) arrondir un resultat

--- fonction.php
+++ fonction.php
@@ -1,14 +1,13 @@
 <?php
-$notes = [15, 10, 13, 9];
+$notes = [20, 10, 13];
 
 $sum = array_sum($notes); // calcule la somme d'un tableau
 $taille = sizeof($notes); // calcule la taille d'un tableau 
 $count = count($notes); // compter le nombre d'élément d'un tableau 
 
-$moy = $sum / $count;
+$moy = round($sum / $count); // arrondir en chiffre rond
+//$moy = round($sum / $count, 2); // arrondir avec deux chiffre après la virgule
 
 echo "La moyenne de vos notes est $moy";
-
-
 
 ?>
