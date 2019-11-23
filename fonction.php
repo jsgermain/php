374b962259cd8abce9117522315a56e60921856d
john <john-88@live.fr>
2019-11-23 12:54:50 +0100

    Fonction (array_reverse) permettant d'inverser l'odre des éléments d'un tableau

--- fonction.php
+++ fonction.php
@@ -1,15 +1,9 @@
 <?php
-$notes = [20, 10, 13];
-array_push($notes,[15, 6]); // Ajouter de nouvel élément dans le tableau
+$notes = [20, 10, 13, 54, 5];
+$notesInverser = array_reverse($notes); // sert à inverser l'ordre des élément dans un tableau
 
-$sum = array_sum($notes); // calcule la somme d'un tableau
-$taille = sizeof($notes); // calcule la taille d'un tableau 
-$count = count($notes); // compter le nombre d'élément d'un tableau 
-
-
-$moy = $sum / $count;
-
-echo "La moyenne de vos notes est $moy";
+print_r($notes);
+print_r($notesInverser);
 
 
 ?>
