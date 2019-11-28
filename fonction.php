85e3aaea702462660edba23ff60f863643cd13f8
john <john-88@live.fr>
2019-11-28 15:19:25 +0100

    Fonction (strlen => pour la taille) et (str_repeat => pour répeter autant souhaité)

--- fonction.php
+++ fonction.php
@@ -1,11 +1,14 @@
 <?php
 
-    $insultes=['merde', 'pute'];
+    $insultes=['merde', 'pute', 'con','moche'];
+
     $phrase=readline("Entrez une phrase :\n");
 
     foreach($insultes as $insulte)
     {
-        $phrase = str_replace($insulte,'****', $phrase);
+        $taille = strlen($insulte); /* sert à calculer la taille d'un mot */
+        $replace = str_repeat('*', $taille); /* sert à répeter * autant de la taille du mot */
+        $phrase = str_replace($insulte,$replace, $phrase);
     }
     echo $phrase;
 /*
