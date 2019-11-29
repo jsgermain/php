4a9b2860185d74ace440d0adaae24f2d871cdb1a
john <john-88@live.fr>
2019-11-29 07:30:20 +0100

    L'utilisation de la fonction

--- fonction.php
+++ fonction.php
@@ -6,7 +6,8 @@ function bonjour($nom)
     echo "Bonjour $nom \n";
 }
 
-
+bonjour('Norane');
+bonjour('Pierre');
 
 /*
 https://www.youtube.com/watch?v=wzDjK4rM67c&list=PLjwdMgw5TTLVDv-ceONHM_C19dPW1MAMD&index=10
