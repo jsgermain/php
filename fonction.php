90965d78cf6a245145384415b850edef886664ac
john <john-88@live.fr>
2019-11-29 07:00:11 +0100

    Fonction (substr) pour la soustraction d'une ou plusieurs lettres

--- fonction.php
+++ fonction.php
@@ -6,8 +6,9 @@
 
     foreach($insultes as $insulte)
     {
-        $taille = strlen($insulte);
-        $asterisque = str_repeat('*', $taille); 
+        $lettre = substr($insulte, 0, 1); /*sert à soustraire la ou les lettre d'un mot */
+        $taille = strlen($insulte) -1; /* on soustraire le nb de mot enlevé dans la (strlen) */
+        $asterisque = $lettre.str_repeat('*', $taille); /* on concatène la var de la lettre soustrait dans la (str_repeat) */
         $phrase = str_replace($insulte,$asterisque, $phrase);
     }
     echo $phrase;
