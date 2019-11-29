fb43219516d85fbc33c4d6ac0896dfdc2711151d
john <john-88@live.fr>
2019-11-29 07:18:32 +0100

    Fonction (stristr) recherchant une chaine

--- fonction.php
+++ fonction.php
@@ -6,10 +6,12 @@
 
     foreach($insultes as $insulte)
     {
-        $lettre = substr($insulte, 0, 1); /*sert à soustraire la ou les lettre d'un mot */
-        $taille = strlen($insulte) -1; /* on soustraire le nb de mot enlevé dans la (strlen) */
-        $asterisque = $lettre.str_repeat('*', $taille); /* on concatène la var de la lettre soustrait dans la (str_repeat) */
-        $phrase = str_replace($insulte,$asterisque, $phrase);
+        if(stristr($phrase, $insulte)) /*sert à chercher la presence d'une var defini ou une lettre*/
+        {
+            $phrase= "Ce message contient des insultes";
+            //echo "Ce message contient des insultes";
+
+        }
     }
     echo $phrase;
 /*
